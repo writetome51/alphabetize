22ae1e590c6d2caa6198c131f3025b0d959ccadc
Steve Thompson <writetome51@gmail.com>
2020-08-21 03:45:03 -0600

    Minor changes to formatting

--- dist/__privy.js
+++ dist/__privy.js
@@ -1,22 +1,30 @@
-import { getArrayCopy } from '@writetome51/get-array-copy';
+import {getArrayCopy} from '@writetome51/get-array-copy';
+
+
 export function getModifiedArrayCopy(array, modifyCopy) {
-    let copy = getArrayCopy(array);
-    if (copy.length < 2)
-        return copy;
-    modifyCopy(copy);
-    return copy;
+	let copy = getArrayCopy(array);
+	if (copy.length < 2) return copy;
+
+	modifyCopy(copy);
+	return copy;
 }
+
+
 export function alphabetizeCaseSensitive(arr, getValueToSortBy) {
-    // First sort so all uppercase letters appear before any lowercase:
-    arr.sort((a, b) => String(getValueToSortBy(a)) < String(getValueToSortBy(b)) ? -1 : 1);
-    alphabetizeCaseInsensitive(arr, getValueToSortBy);
+	// First sort so all uppercase letters appear before any lowercase:
+	arr.sort((a, b) => String(getValueToSortBy(a)) < String(getValueToSortBy(b)) ? -1 : 1);
+	alphabetizeCaseInsensitive(arr, getValueToSortBy);
 }
+
+
 export function alphabetizeCaseInsensitive(arr, getValueToSortBy) {
-    arr.sort(getCaseInsensitiveComparator(getValueToSortBy));
+	arr.sort(getCaseInsensitiveComparator(getValueToSortBy));
 }
+
+
 export function getCaseInsensitiveComparator(getValueToSortBy) {
-    return (a, b) => {
-        return (String(getValueToSortBy(a)).toLowerCase()
-            < String(getValueToSortBy(b)).toLowerCase() ? -1 : 1);
-    };
+	return (a, b) => {
+		return (String(getValueToSortBy(a)).toLowerCase()
+		< String(getValueToSortBy(b)).toLowerCase() ? -1 : 1);
+	};
 }
