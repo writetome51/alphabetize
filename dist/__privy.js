fccca4bb09475ef40deae450b7b159f4aa62a618
Steve Thompson <writetome51@gmail.com>
2020-08-21 17:18:20 -0600

    Minor formatting changes

--- dist/__privy.js
+++ dist/__privy.js
@@ -1,11 +1,20 @@
 // Repeated tests show Array.sort((a, b) => a < b ? -1 : 1) is faster than quicksort algorithms for
 // alphabetical sorting.
+
 export function getCaseInsensitiveComparator(getValueToSortBy) {
-    return getComparator((a, b) => String(getValueToSortBy(a)).toLowerCase() < String(getValueToSortBy(b)).toLowerCase());
+	return getComparator(
+		(a, b) => String(getValueToSortBy(a)).toLowerCase() < String(getValueToSortBy(b)).toLowerCase()
+	);
 }
+
+
 export function getCaseSensitiveComparator(getValueToSortBy) {
-    return getComparator((a, b) => String(getValueToSortBy(a)) < String(getValueToSortBy(b)));
+	return getComparator(
+		(a, b) => String(getValueToSortBy(a)) < String(getValueToSortBy(b))
+	);
 }
+
+
 export function getComparator(getBoolExpression) {
-    return (a, b) => (getBoolExpression(a, b) ? -1 : 1);
+	return (a, b) => (getBoolExpression(a, b) ? -1 : 1);
 }
--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,16 @@
-import { getCaseInsensitiveComparator, getCaseSensitiveComparator } from './__privy.js';
+import {getCaseInsensitiveComparator, getCaseSensitiveComparator} from './__privy.js';
+
+
 // Uppercase letters are ordered before their lowercase versions, i.e., ['A','a','AA','aa']
+
 export function alphabetize(arr, getValueToSortBy = (element) => element) {
-    // First sort so all uppercase letters appear before any lowercase:
-    arr.sort(getCaseSensitiveComparator(getValueToSortBy));
-    alphabetizeInsensitive(arr, getValueToSortBy);
+	// First sort so all uppercase letters appear before any lowercase:
+	arr.sort(getCaseSensitiveComparator(getValueToSortBy));
+
+	alphabetizeInsensitive(arr, getValueToSortBy);
 }
+
+
 export function alphabetizeInsensitive(arr, getValueToSortBy = (element) => element) {
-    arr.sort(getCaseInsensitiveComparator(getValueToSortBy));
+	arr.sort(getCaseInsensitiveComparator(getValueToSortBy));
 }
